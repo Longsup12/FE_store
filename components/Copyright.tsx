3d8083743ab626cf18d62ce71a273ead32ab0787
Longsup12 <65186477+Longsup12@users.noreply.github.com>
2024-06-27 10:51:15 +0700

    commit

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import localFont from "next/font/local"
+
+const customFont = localFont({src: '../public/fonts/MoonLight.otf'})
 
 const Copyright = () => {
   return (
-    <div className="flex flex-row justify-between items-center py-5 bg-main-violet">
+    <div className={customFont.className}>
+     <div className="flex flex-row justify-between items-center py-5 bg-main-violet">
         <p className="w-full text-center text-grey-2">Copyright © 2024 Huyenne_Blog</p>
         <div className=""></div>
     </div>
+    </div>
+   
   )
 }
 
